29ed9ce4feff7296e02e0ffec29fb28bb61c5d19
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2025-01-24 12:09:01 +0300

    Placed arrow-icon into the UserItem-composable

--- app/src/main/java/com/example/githubsearchapp/presentation/searchScreen/components/UserItem.kt
+++ app/src/main/java/com/example/githubsearchapp/presentation/searchScreen/components/UserItem.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,8 +17,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -76,6 +80,16 @@ fun UserItem(userState: SearchListItemState.UserState) {
                     modifier = Modifier.weight(1f),
                     textAlign = TextAlign.Center,
                     overflow = TextOverflow.Ellipsis
+                )
+
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.arrow_right_icon),
+                    contentDescription = null,
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .weight(0.5f)
+                        .size(50.dp)
+                        .padding(4.dp),
                 )
             }
         }
